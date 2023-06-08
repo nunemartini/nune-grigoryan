090b432c584f654cc2837eb1488789d4ad851d1d
nunemartini <135968467+nunemartini@users.noreply.github.com>
2023-06-08 14:00:54 +0300

    initial commit

---
+++ Elevator.java
@@ -0,0 +1,37 @@
+public class Elevator {
+    public int minFloor;
+    public int maxFloor;
+    public int currentFloor = 1;
+
+    public Elevator(int minFloor, int maxFloor){
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+    public void moveUp(){
+        currentFloor = getCurrentFloor() + 1;
+    }
+    public void moveDown(){
+        currentFloor = getCurrentFloor() - 1;
+    }
+    public int getCurrentFloor(){
+        return currentFloor;
+    }
+    public void move(int floor){
+        if (floor > getCurrentFloor() && floor <=maxFloor){
+            while (floor != currentFloor){
+                moveUp();
+                System.out.println(currentFloor);
+            }
+        } else if (floor < getCurrentFloor() && floor >=minFloor){
+            while (floor != currentFloor){
+                moveDown();
+                System.out.println(currentFloor);
+            }
+        } else {
+            System.out.println("Что-то пошло не так:(");
+            return;
+        }
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Elevator elevator1 = new Elevator(1, 25);
+        while (true){
+            elevator1.move(9);
+        }
+    }
+}
